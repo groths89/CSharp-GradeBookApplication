c3fc8e77d026332ce510a7ede35db466052d87b2
Gregory Rothstein <gregory@thecommodoredesigns.com>
2020-06-14 14:28:14 -0400

    Module 5 Checkpoint:fix completion of module

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -40,19 +40,19 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
-            switch (parts[2])
+            var type = parts[2].ToLower();
+            BaseGradeBook gradeBook;
+            if (type == "standard")
+                gradeBook = new StandardGradeBook(name);
+            else if (type == "ranked")
+                gradeBook = new RankedGradeBook(name);
+            else
             {
-                case "standard":
-                    gradeBook = new StandardGradeBook(name);
-                    break;
-                case "ranked":
-                    gradeBook = new RankedGradeBook(name);
-                    break;
-                default:
-                    Console.WriteLine(name + " is not a supported type of gradebook, please try again.");
-                    break;
+                Console.WriteLine("{0} is not a supported type of gradebook, please try again.", type);
+                return;
             }
+                
+
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
