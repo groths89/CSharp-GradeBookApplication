6787631da1c044cb8a1d8a96d4ee40bb0dc58d97
Gregory Rothstein <gregory@thecommodoredesigns.com>
2020-06-14 13:09:47 -0400

    Module 4 Chsckpoint:fix

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -31,23 +31,26 @@ namespace GradeBook.GradeBooks
                 return 'F';
         }
 
-        public char CalculateStatistics(double averageGrade)
+        public override void CalculateStatistics()
         {
             if (Students.Count < 5)
-                throw new InvalidOperationException(
-                    "Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
 
             base.CalculateStatistics();
-            return 'F';
         }
 
-        public string CalculateStudentStatistics()
+        public override void CalculateStudentStatistics(string name)
         {
             if (Students.Count < 5)
-                throw new InvalidOperationException(
-                    "Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
-            base.CalculateStudentStatistics(Name);
-            return Name;
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStudentStatistics(name);
         }
     }
 }
